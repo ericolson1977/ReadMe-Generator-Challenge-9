5fed5662fb6636dc140bafadfd16f63ab3c07105
ericolson1977 <mr_tues_night@yahoo.com>
2023-10-11 23:42:30 -0500

    created a module for the functions to generate the license badge, link and info section. also contains function to create the content of the readme file. created a fucntion to tak user input and generate the readme file.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const { log } = require('console');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -16,57 +19,62 @@ const questions = [
     {
         type: 'input',
         message: "What is your project's name?",
-        name: 'projectname',
+        // name: 'projectname',
     },
     {
         type: 'input',
         message: 'Please write a short description of your project',
-        name: 'description',
+        // name: 'description',
     },
     {
         type: 'list',
         message: 'What kind of license should your project have?',
-        choices: ['APACHE 2.0', 'MIT', 'BSD 3', 'Mozilla 2.0'],
-        name: 'license',
+        choices: ['APACHE 2.0', 'MIT', 'BSD 3', 'Mozilla 2.0', 'None'],
+        // name: 'license',
     },
     {
         type: 'input',
         message: 'what command should be run to install dependencies?',
         default: 'npm i',
-        name: 'cmddependencies',
+        // name: 'cmddependencies',
     },
     {
         type: 'input',
         message: 'What command should be run to run tests?',
         default: 'npm test',
-        name: 'cmdtests',
+        // name: 'cmdtests',
     },
     {
         type: 'input',
         message: 'What does the user need to know about using the repo?',
-        name: 'userepo',
+        // name: 'userepo',
     },
     {
         type: 'input',
         message: 'What does the user need to know about contributing to the repo?',
-        name: 'contribute',
+        // name: 'contribute',
     },
 
 ];
 
 // TODO: Create a function to write README file
-// fs.writeFile('README.md', data, function(error){
-    
-// })
+
 
 // TODO: Create a function to initialize app
 function init() { 
     inquirer
   .prompt(questions)
-  .then(function(answers){
-    console.log(answers);
-})
-};
+  .then((answers) => {
+    const markdown = generateMarkdown(answers);
+    fs.writeFile('README.md', markdown, function (error) {
+        if (error) {
+        console.log(error);
+        } else {
+        console.log('Generating README...');
+        }
+    })
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](LICENSE)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `[License](LICENSE)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is licensed under the ${license} license. See the [${license}](LICENSE) file for more details.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+  return `# ${data.projectname}
 
+## Description
+${data.description}
+
+${licenseBadge}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [How to Contribute](#howtocontribute)
+5. [Tests](#tests)
+6. [Questions](#questions)  
+
+## Installation
+To install the necessary dependecies, run the following command:
+${data.cmddependecies}
+
+## Usage
+${data.userepo}
+
+## License
+${licenseSection}
+For the full license text, please see [${licenseLink}](LICENSE).
+
+## How to Contribute
+${data.contribute}
+
+## Tests
+To run tests, run the following command:
+${data.cmdtests}
+
+## Questions
+if you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.username}.
 `;
 }
 
